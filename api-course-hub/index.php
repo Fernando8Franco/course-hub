83730684d8192bb2a2708b98a57cf75a5c95ecd3
Fernando8Franco <francoduranluisfernando@gmail.com>
2024-04-08 14:44:02 -0600

    Fixing routes

--- api-course-hub/index.php
+++ api-course-hub/index.php
@@ -14,14 +14,19 @@ $user = new Services\UserService;
 
 //USER ROUTES
 Flight::route('GET /users', [$user, 'getAll']);
-Flight::route('GET /user', [$user, 'getUserData']);
 Flight::route('GET /user/@id', [$user, 'getOne']);
-Flight::route('POST /users', [$user, 'create']);
+Flight::route('GET /user', [$user, 'getUserData']);
+
 Flight::route('POST /auth', [$user, 'auth']);
+Flight::route('POST /user-admin', [$user, 'createAdmin']);
+Flight::route('POST /user-costumer', [$user, 'createCostumer']);
 Flight::route('POST /send-reset-password-email', [$user, 'sendResetPasswordEmail']);
-Flight::route('POST /reset-password/', [$user, 'updatePasswordByToken']);
-Flight::route('PUT /user/@id', [$user, 'update']);
-Flight::route('PUT /update-password/@id', [$user, 'updatePassword']);
+Flight::route('POST /reset-password', [$user, 'resetPassword']);
+
+Flight::route('PUT /user-admin/@id', [$user, 'update']);
+Flight::route('PUT /user-costumer', [$user, 'updateCostumer']);
+Flight::route('PUT /update-password', [$user, 'updatePassword']);
+
 Flight::route('DELETE /user/@id', [$user, 'delete']);
 
 Flight::start();
--- api-course-hub/services/UserService.php
+++ api-course-hub/services/UserService.php
@@ -8,7 +8,9 @@ use Exception;
 class UserService {
 
     function getAll() {
-        if (tokenData()->user_type != 'ADMIN')
+        $token_data = tokenData();
+
+        if ($token_data->user_type != 'ADMIN')
             Flight::halt(403, json_encode(['status' => 'error', 'message' => 'Unauthorized request']));
       
         try {
@@ -25,7 +27,9 @@ class UserService {
     }
 
     function getOne($id) {
-        if (tokenData()->user_type != 'ADMIN')
+        $token_data = tokenData();
+
+        if ($token_data->user_type != 'ADMIN')
             Flight::halt(403, json_encode(['status' => 'error', 'message' => 'Unauthorized request']));
         
         try {
@@ -43,13 +47,18 @@ class UserService {
     }
 
     function getUserData() {
+        $token_data = tokenData();
+
         try {
-            $stmt = Flight::db()->prepare("SELECT id, name, father_last_name, mother_last_name, phone_number, email FROM user WHERE id = ?");
-            $stmt->bind_param('i', tokenData()->user_id);
+            $stmt = Flight::db()->prepare("SELECT id, name, father_last_name, mother_last_name, phone_number, email FROM user WHERE id = ? AND is_active = 1");
+            $stmt->bind_param('i', $token_data->user_id);
             $stmt->execute();
             $result = $stmt->get_result()->fetch_assoc();
             $stmt->close();
             Flight::db()->close();
+
+            if (is_null($result))
+                Flight::halt(400, json_encode(['status' => 'error', 'message' => 'The user is not active']));
 
             Flight::json($result, 200);
         } catch (Exception $e) {
@@ -57,8 +66,34 @@ class UserService {
         }
     }
 
-    function create() {
-        if (tokenData()->user_type != 'ADMIN')
+    function auth() {
+        try {
+            $data = Flight::request()->data;
+            $email = $data->email;
+            $password = $data->password;
+        
+            $stmt = Flight::db()->prepare("SELECT password, id, user_type FROM user WHERE email = ? AND is_active = 1;");
+            $stmt->bind_param('s', $email);
+            $stmt->execute();
+            $result = $stmt->get_result()->fetch_assoc();
+            $stmt->close();
+            Flight::db()->close();
+        
+            if (is_null($result) || !password_verify($password, $result['password'])) 
+                Flight::halt(400, json_encode(['status' => 'error', 'message' => 'Wrong password or user']));
+            
+            $token = encodeToken($result['id'], $result['user_type']);
+        
+            Flight::json(array('token' => $token), 200);
+        } catch (Exception $e) {
+            Flight::json(array('status' => 'error', 'message' => $e->getMessage()), 400);
+        }
+    }
+
+    function createAdmin() {
+        $token_data = tokenData();
+
+        if ($token_data->user_type != 'ADMIN')
             Flight::halt(403, json_encode(['status' => 'error', 'message' => 'Unauthorized request']));
       
         try {
@@ -70,8 +105,16 @@ class UserService {
             $birthday = $data->birthday;
             $phone_number = $data->phone_number;
             $email = $data->email;
-            $user_type = $data->user_type;
+            $user_type = 'ADMIN';
             $is_active = $data->is_active;
+
+            $stmt = Flight::db()->prepare("SELECT EXISTS (SELECT id FROM user WHERE email = ?) as email_exist");
+            $stmt->bind_param('s', $email);
+            $stmt->execute();
+            $result = $stmt->get_result()->fetch_assoc();
+
+            if ($result['email_exist']) 
+                Flight::halt(400, json_encode(['status' => 'warning', 'message' => 'The email is already registered']));
         
             $enc_password = password_hash($password, PASSWORD_DEFAULT);
         
@@ -88,66 +131,37 @@ class UserService {
         }
     }
 
-    function update($id) {
-        if (tokenData()->user_type != 'ADMIN')
-            Flight::halt(403, json_encode(['status' => 'error', 'message' => 'Unauthorized request']));
-        
+    function createCostumer() {
         try {
             $data = Flight::request()->data;
             $name = $data->name;
             $father_last_name = $data->father_last_name;
             $mother_last_name = $data->mother_last_name;
+            $password = $data->password;
             $birthday = $data->birthday;
             $phone_number = $data->phone_number;
             $email = $data->email;
-            $user_type = $data->user_type;
+            $user_type = 'COSTUMER';
             $is_active = $data->is_active;
-        
-            $stmt = Flight::db()->prepare("UPDATE user SET name = ?, father_last_name = ?, mother_last_name = ?, birthday = ?, phone_number = ?, email = ?, user_type = ?, is_active = ?
-            WHERE id = ?");
-            $stmt->bind_param('sssssssii', $name, $father_last_name, $mother_last_name, $birthday, $phone_number, $email, $user_type, $is_active, $id);
-            $stmt->execute();
-            $rows = $stmt->affected_rows;
-            $stmt->close();
-            Flight::db()->close();
 
-            if ($rows == 0)
-                Flight::halt(200, json_encode(['status' => 'warning', 'message' => "No changes were made to the user with id {$id}"]));
-        
-            Flight::json(array('status' => 'success', 'message' => 'User changed correctly'), 200);
-            
-        } catch (Exception $e) {
-            Flight::json(array('status' => 'error', 'message' => $e->getMessage()), 400);
-        }
-    }
-
-    function updatePassword($id) {
-        if (tokenData()->user_id != $id)
-            Flight::halt(403, json_encode(['status' => 'error', 'message' => 'Unauthorized request']));
-
-        try {
-            $data = Flight::request()->data;
-            $old_password = $data->old_password;
-            $new_password = $data->new_password;
-
-            $stmt = Flight::db()->prepare("SELECT password FROM user WHERE id = ? AND is_active = 1;");
-            $stmt->bind_param('s', $id);
+            $stmt = Flight::db()->prepare("SELECT EXISTS (SELECT id FROM user WHERE email = ?) as email_exist");
+            $stmt->bind_param('s', $email);
             $stmt->execute();
             $result = $stmt->get_result()->fetch_assoc();
 
-            if (is_null($result) || !password_verify($old_password, $result['password'])) 
-                Flight::halt(400, json_encode(['status' => 'error', 'message' => 'Wrong password']));
-
-            $enc_password = password_hash($new_password, PASSWORD_DEFAULT);
+            if ($result['email_exist']) 
+                Flight::halt(400, json_encode(['status' => 'warning', 'message' => 'The email is already registered']));
         
-            $stmt = Flight::db()->prepare("UPDATE user SET password = ? WHERE id = ?");
-            $stmt->bind_param('si', $enc_password, $id);
+            $enc_password = password_hash($password, PASSWORD_DEFAULT);
+        
+            $stmt = Flight::db()->prepare("INSERT INTO user (name, father_last_name, mother_last_name, password, birthday, phone_number, email, user_type, is_active)
+            VALUES (?,?,?,?,?,?,?,?,?);");
+            $stmt->bind_param('ssssssssi', $name, $father_last_name, $mother_last_name, $enc_password, $birthday, $phone_number, $email, $user_type, $is_active);
             $stmt->execute();
             $stmt->close();
             Flight::db()->close();
         
-            Flight::json(array('status' => 'success', 'message' => 'User password changed correctly'), 200);
-            
+            Flight::json(array('status' => 'success', 'message' => 'User stored correctly'), 200);
         } catch (Exception $e) {
             Flight::json(array('status' => 'error', 'message' => $e->getMessage()), 400);
         }
@@ -157,16 +171,34 @@ class UserService {
         try {
             $data = Flight::request()->data;
             $email = $data->email;
+
+            $stmt = Flight::db()->prepare("SELECT last_reset_request FROM user WHERE email = ?");
+            $stmt->bind_param('s', $email);
+            $stmt->execute();
+            $result = $stmt->get_result()->fetch_assoc();
+
+            if (!empty($result['last_reset_request'])) {
+                $last_reset_time = strtotime($result['last_reset_request']);
+                $current_time = time();
+                $time_difference = $current_time - $last_reset_time;
+
+                if ($time_difference < 600) {
+                    $wait_time = 600 - $time_difference;
+                    Flight::halt(400, json_encode(['status' => 'warning', 'message' => $wait_time]));
+                }
+            }
+
             $token = bin2hex(random_bytes(16));
             $token_hash = hash('sha256', $token);
             $expiry = date('Y-m-d H:i:s', time() + 10000);
+            $reset_request = date('Y-m-d H:i:s', time());
         
-            $stmt = Flight::db()->prepare("UPDATE user SET reset_token_hash = ?, reset_token_expires_at = ? WHERE email = ?");
-            $stmt->bind_param('sss', $token_hash, $expiry, $email);
+            $stmt = Flight::db()->prepare("UPDATE user SET reset_token_hash = ?, reset_token_expires_at = ?, last_reset_request = ? WHERE email = ?");
+            $stmt->bind_param('ssss', $token_hash, $expiry, $reset_request, $email);
             $stmt->execute();
 
             if ($stmt->affected_rows == 0)
-                Flight::halt(400, json_encode(['status' => 'warning', 'message' => 'Wrong Email']));
+                Flight::halt(200, json_encode(['status' => 'success', 'message' => 'Email send correctly']));
 
             try {
                 $mail = require "MailerService.php";
@@ -194,7 +226,7 @@ class UserService {
         }
     }
 
-    function updatePasswordByToken() {
+    function resetPassword() {
         try {
             $data = Flight::request()->data;
             $token = $data->token;
@@ -223,8 +255,113 @@ class UserService {
         }
     }
 
+    function update($id) {
+        $token_data = tokenData();
+
+        if ($token_data->user_type != 'ADMIN')
+            Flight::halt(403, json_encode(['status' => 'error', 'message' => 'Unauthorized request']));
+        
+        try {
+            $data = Flight::request()->data;
+            $name = $data->name;
+            $father_last_name = $data->father_last_name;
+            $mother_last_name = $data->mother_last_name;
+            $birthday = $data->birthday;
+            $phone_number = $data->phone_number;
+            $email = $data->email;
+            $is_active = $data->is_active;
+
+            $stmt = Flight::db()->prepare("SELECT EXISTS (SELECT id FROM user WHERE email = ? AND id != ?) as email_exist");
+            $stmt->bind_param('si', $email, $id);
+            $stmt->execute();
+            $result = $stmt->get_result()->fetch_assoc();
+
+            if ($result['email_exist']) 
+                Flight::halt(400, json_encode(['status' => 'warning', 'message' => 'The email is already registered']));
+        
+            $stmt = Flight::db()->prepare("UPDATE user SET name = ?, father_last_name = ?, mother_last_name = ?, birthday = ?, phone_number = ?, email = ?, is_active = ?
+            WHERE id = ?");
+            $stmt->bind_param('ssssssii', $name, $father_last_name, $mother_last_name, $birthday, $phone_number, $email, $is_active, $id);
+            $stmt->execute();
+            $stmt->close();
+            Flight::db()->close();
+        
+            Flight::json(array('status' => 'success', 'message' => 'User changed correctly'), 200);
+        } catch (Exception $e) {
+            Flight::json(array('status' => 'error', 'message' => $e->getMessage()), 400);
+        }
+    }
+
+    function updateCostumer() {
+        $token_data = tokenData();
+
+        if ($token_data->user_type != 'COSTUMER')
+            Flight::halt(403, json_encode(['status' => 'error', 'message' => 'Unauthorized request']));
+        
+        try {
+            $data = Flight::request()->data;
+            $name = $data->name;
+            $father_last_name = $data->father_last_name;
+            $mother_last_name = $data->mother_last_name;
+            $birthday = $data->birthday;
+            $phone_number = $data->phone_number;
+            $email = $data->email;
+
+            $stmt = Flight::db()->prepare("SELECT EXISTS (SELECT id FROM user WHERE email = ? AND id != ?) as email_exist");
+            $stmt->bind_param('si', $email, $token_data->user_id);
+            $stmt->execute();
+            $result = $stmt->get_result()->fetch_assoc();
+
+            if ($result['email_exist']) 
+                Flight::halt(400, json_encode(['status' => 'warning', 'message' => 'The email is already registered']));
+        
+            $stmt = Flight::db()->prepare("UPDATE user SET name = ?, father_last_name = ?, mother_last_name = ?, birthday = ?, phone_number = ?, email = ?
+            WHERE id = ?");
+            $stmt->bind_param('ssssssi', $name, $father_last_name, $mother_last_name, $birthday, $phone_number, $email, $token_data->user_id);
+            $stmt->execute();
+            $stmt->close();
+            Flight::db()->close();
+        
+            Flight::json(array('status' => 'success', 'message' => 'User changed correctly'), 200);
+            
+        } catch (Exception $e) {
+            Flight::json(array('status' => 'error', 'message' => $e->getMessage()), 400);
+        }
+    }
+
+    function updatePassword() {
+        $token_data = tokenData();
+
+        try {
+            $data = Flight::request()->data;
+            $old_password = $data->old_password;
+            $new_password = $data->new_password;
+
+            $stmt = Flight::db()->prepare("SELECT password FROM user WHERE id = ? AND is_active = 1;");
+            $stmt->bind_param('s', $token_data->user_id);
+            $stmt->execute();
+            $result = $stmt->get_result()->fetch_assoc();
+
+            if (is_null($result) || !password_verify($old_password, $result['password'])) 
+                Flight::halt(400, json_encode(['status' => 'error', 'message' => 'Wrong password']));
+
+            $enc_password = password_hash($new_password, PASSWORD_DEFAULT);
+        
+            $stmt = Flight::db()->prepare("UPDATE user SET password = ? WHERE id = ?");
+            $stmt->bind_param('si', $enc_password, $token_data->user_id);
+            $stmt->execute();
+            $stmt->close();
+            Flight::db()->close();
+        
+            Flight::json(array('status' => 'success', 'message' => 'User password changed correctly'), 200);
+        } catch (Exception $e) {
+            Flight::json(array('status' => 'error', 'message' => $e->getMessage()), 400);
+        }
+    }
+
     function delete($id) {
-        if (tokenData()->user_type != 'ADMIN')
+        $token_data = tokenData();
+        if ($token_data->user_type != 'ADMIN')
             Flight::halt(403, json_encode(['status' => 'error', 'message' => 'Unauthorized request']));
         
         try {
@@ -239,30 +376,6 @@ class UserService {
                 Flight::halt(200, json_encode(['status' => 'warning', 'message' => "The user with id: {$id} dont exist"]));
         
             Flight::json(array('status' => 'success', 'message' => 'User deleted correctly'), 200);
-        } catch (Exception $e) {
-            Flight::json(array('status' => 'error', 'message' => $e->getMessage()), 400);
-        }
-    }
-
-    function auth() {
-        try {
-            $data = Flight::request()->data;
-            $email = $data->email;
-            $password = $data->password;
-        
-            $stmt = Flight::db()->prepare("SELECT password, id, user_type FROM user WHERE email = ? AND is_active = 1;");
-            $stmt->bind_param('s', $email);
-            $stmt->execute();
-            $result = $stmt->get_result()->fetch_assoc();
-            $stmt->close();
-            Flight::db()->close();
-        
-            if (is_null($result) || !password_verify($password, $result['password'])) 
-                Flight::halt(400, json_encode(['status' => 'error', 'message' => 'Wrong password or user']));
-            
-            $token = encodeToken($result['id'], $result['user_type']);
-        
-            Flight::json(array('token' => $token), 200);
         } catch (Exception $e) {
             Flight::json(array('status' => 'error', 'message' => $e->getMessage()), 400);
         }
